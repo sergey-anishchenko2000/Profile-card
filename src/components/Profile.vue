fc419f62c44283541638ae1af424b25af7646ddf
jsec1912 <goodhelper21@gmail.com>
2020-12-25 04:27:06 +0900

    first commit

---
+++ src/components/Profile.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="background">
+        <div class="container">
+            <div class="card">
+                <div class="card-header">
+                </div>
+                <div>
+                    <div class="profile">
+                        <div >
+                            <img class="profile_img" src="/assets/image-victor.jpg" />
+                            <p class="profile_name"> Vitor Crest                                 
+                                <span class="profile_age" > 26 </span> 
+                            </p>
+                            <p class="profile_city"> London </p>
+                        </div>
+                    </div>
+                    <hr class="divider">
+                    <div class="content">
+                        <div class="show">
+                            <p> 
+                                <span class="label_number"> 80K </span>
+                                <span class="label_detail"> Followers </span>
+                            </p>
+                            <p> 
+                                <span class="label_number"> 803K </span>
+                                <span class="label_detail"> Likes </span>
+                            </p>
+                            <p> 
+                                <span class="label_number"> 1.4K </span>
+                                <span class="label_detail"> Photos </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
